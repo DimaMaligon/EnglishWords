c24c47779dc4c3d3e54294a621df20d6f8e748b7
maligonD <maligond42gmai.com>
2023-05-11 11:14:32 +0300

    add new navgraph and screen

--- app/src/main/java/com/example/englishwords/navigation/NavGraphLetter.kt
+++ app/src/main/java/com/example/englishwords/navigation/NavGraphLetter.kt
@@ -7,12 +7,15 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import androidx.navigation.navigation
+import com.example.englishwords.screens.EnterWordsScreen
 import com.example.englishwords.screens.ErrorScreen
 import com.example.englishwords.screens.LetterListScreen
 import com.example.englishwords.screens.LetterScreen
 import com.example.englishwords.viewmodel.LetterViewModel
 
 fun NavGraphBuilder.navGraphLetter(navController: NavHostController, listViewModel: ArrayList<ViewModel>){
+    val viewModelLetter = listViewModel[0] as LetterViewModel
+
     navigation(
         startDestination = Screens.LetterList.route,
         route = LETTER_ROUTE
@@ -33,8 +36,14 @@ fun NavGraphBuilder.navGraphLetter(navController: NavHostController, listViewMod
             val letterFromList = it.arguments?.getString(DETAIL_ARGUMENT_LETTER) ?: ""
             when(letterFromList){
                 ""-> ErrorScreen(navController)
-                else -> LetterScreen(navController, letterFromList, listViewModel.get(0) as LetterViewModel)
+                else -> LetterScreen(navController, letterFromList, viewModelLetter)
             }
+        }
+
+        composable(
+            route = Screens.EnterWordsScreen.route
+        ) {
+            EnterWordsScreen(navController, viewModelLetter)
         }
     }
 

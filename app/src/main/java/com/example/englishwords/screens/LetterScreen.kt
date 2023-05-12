ed0c86b720cf4fc0eb0ec96093419916867e4352
maligonD <maligond42gmai.com>
2023-05-12 09:09:44 +0300

    add textfield search

--- app/src/main/java/com/example/englishwords/screens/LetterScreen.kt
+++ app/src/main/java/com/example/englishwords/screens/LetterScreen.kt
@@ -1,20 +1,24 @@
 package com.example.englishwords.screens
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.surfaceColorAtElevation
@@ -23,6 +27,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -39,68 +45,76 @@ fun LetterScreen(
     letter: String?,
     letterViewModel: LetterViewModel
 ) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = stringResource(id = R.string.app_name),
-                        Modifier.padding(),
-                        fontFamily = fontPlayfair,
-                        fontWeight = FontWeight.Normal
-                    )
-                },
-                colors = TopAppBarDefaults.smallTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
-                ),
-                actions = {
-                    IconButton(onClick = {
-                        navController.navigate(Screens.EnterWordsScreen.route)
-                    }) {
-                        Icon(Icons.Filled.Add, stringResource(id = R.string.title_icon_enter_words))
-                    }
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = stringResource(id = R.string.app_name),
+                            Modifier.padding(),
+                            fontFamily = fontPlayfair,
+                            fontWeight = FontWeight.Normal
+                        )
+                    },
+                    colors = TopAppBarDefaults.smallTopAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp)
+                    ),
+                    actions = {
+                        IconButton(onClick = {
+                            navController.navigate(Screens.EnterWordsScreen.route)
+                        }) {
+                            Icon(
+                                Icons.Filled.Add,
+                                stringResource(id = R.string.title_icon_enter_words)
+                            )
+                        }
 
-                },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        navController.popBackStack()
-                    }) {
-                        Icon(Icons.Filled.ArrowBack, stringResource(id = R.string.title_icon_back))
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            navController.popBackStack()
+                        }) {
+                            Icon(
+                                Icons.Filled.ArrowBack,
+                                stringResource(id = R.string.title_icon_back)
+                            )
+                        }
                     }
-                }
-            )
-        },
-        content = { padding ->
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.padding(100.dp)
-            ) {
-                Row() {
+                )
+            },
+            content = { padding ->
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(100.dp)
+                ) {
+                    TitleWords(letterViewModel = letterViewModel)
+                    Search()
                     Words(letterViewModel = letterViewModel, letterFrom = letter)
                 }
-            }
 
-        },
-        containerColor = MaterialTheme.colorScheme.background
-    )
+            },
+            containerColor = MaterialTheme.colorScheme.background
+        )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Words(letterViewModel: LetterViewModel, letterFrom: String?) {
+fun TitleWords(letterViewModel: LetterViewModel) {
     letterViewModel.apply {
         val letterScreen by letter.collectAsState()
+        Column(modifier = Modifier.padding(bottom = 15.dp)) {
+            val titleScreen = stringResource(R.string.title_screen_letter)
+            Text(
+                text = "$titleScreen $letterScreen",
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}@Composable
+fun Words(letterViewModel: LetterViewModel, letterFrom: String?) {
+    letterViewModel.apply {
         val englishList by englishList.collectAsState()
-
         setLetter(letterFrom)
         Column() {
-            Column() {
-                val titleScreen = stringResource(R.string.title_screen_letter)
-                Text(
-                    text = "$titleScreen $letterScreen",
-                    style = MaterialTheme.typography.titleMedium
-                )
-            }
             getEnglishList()
             WordList(listWords = englishList)
         }
@@ -112,16 +126,62 @@ fun WordList(listWords: List<String>) {
     LazyColumn(
         modifier = Modifier
             .padding(8.dp)
-            .height(120.dp)
+            .fillMaxSize()
 
     ) {
         items(listWords) {
             Text(
                 text = it,
-                modifier = Modifier.padding(vertical = 10.dp)
+                modifier = Modifier
+                    .padding(vertical = 10.dp)
+                    .shadow(15.dp),
+                maxLines = 3
             )
 
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Search() {
+    Column() {
+        TextField(
+            value = "",
+            onValueChange = { value ->
+                value
+            },
+            leadingIcon = {
+                Icon(
+                    Icons.Default.Search,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(24.dp)
+                )
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                    }
+                ) {
+                    Icon(
+                        Icons.Default.Close,
+                        contentDescription = "",
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .size(24.dp)
+                    )
+                }
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                disabledTextColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            singleLine = true,
+            shape = MaterialTheme.shapes.medium
+        )
     }
 }
 
